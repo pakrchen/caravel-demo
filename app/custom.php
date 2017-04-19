4297d12e10f821d7a202f1fe7c2b5868930bb116
pakrchen <pakrchen@gmail.com>
2017-04-19 13:05:30 +0800

    add before & after

--- app/custom.php
+++ app/custom.php
@@ -61,7 +61,22 @@ App::error(function(Exception $e, App $app) {
 
 });
 
-App::filter(function() {
+/*
+|--------------------------------------------------------------------------
+| Application Filters
+|--------------------------------------------------------------------------
+|
+| Below you will find the "before" and "after" events for the application
+| which may be used to do any work before or after a request into your
+| application. Here you may also register your custom route filters.
+|
+*/
+
+App::before(function() {
+
+});
+
+App::after(function($response) {
 
 });
 
